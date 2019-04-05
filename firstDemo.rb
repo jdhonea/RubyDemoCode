0d59606693ea3b42a47ef0aed6517c489b1172cc
Jason Honea <jhonea129@gmail.com>
2019-04-05 14:21:40 -0500

    Add files via upload

---
+++ firstDemo.rb
@@ -0,0 +1,22 @@
+# puts "Hello there! Please enter a number to square: "
+# number = gets.chomp.to_i
+
+# def square(number)
+#     squared = number * number
+#     return squared.to_s
+# end
+
+# puts "Your number is: " + number.to_s
+# puts "Your number squared is: " + square(number) 
+
+# def square(number)
+#     squaredNum = number*number
+#     return squaredNum
+# end
+number = 10
+lessThan = number < 5
+if lessThan
+    puts "I'm less than!"
+else
+    puts "I'm not less than!"
+end
